9a724c0d8f0570efe57c1a5fe6eeececd6d75588
PatelPanth <146762440+PatelPanth@users.noreply.github.com>
2024-02-26 17:07:22 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var player;
+var seeker;
+
+
+
+function preload(){
+
+}
+
+function setup() {
+
+    // create the canvas for the game
+
+createCanvas(600,200);
+}
+
+function draw() {
+ 
+    // create the sprites
+ 
+  drawSprites();
+}
+
+if (keyDown "space") {
+  player.velocityY=player.velocityY +1
+} 
+  
